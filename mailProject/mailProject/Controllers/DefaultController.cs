624cc70ea64545a41b8982245326e371f5a29b57
Serkan <68036775+SerkanBahtiyar@users.noreply.github.com>
2024-03-06 03:34:27 +0300

    send mail

---
+++ mailProject/mailProject/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+using mailProject.Models;
+
+namespace mailProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Iletisim(Email model)
+        {
+
+            
+            
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("serkanbahtiyar701@gmail.com", "wfhmpjjwtksapadm"),
+                EnableSsl = true,
+            };
+            MailMessage mailim = new MailMessage();
+            mailim.To.Add("serkanbahtiyar701@gmail.com");
+            mailim.From = new MailAddress("serkanbahtiyar701@gmail.com");
+            mailim.Subject = model.baslik;
+            mailim.Body ="İsim: "+model.ad+"<br>"+ "soyad: "+model.soyad +"<br>" + model.icerik;
+            mailim.IsBodyHtml = true;
+            
+            
+            smtpClient.Send(mailim);
+         
+            return PartialView();
+        }
+   
+    }
+}
---
+++ mailProject/mailProject/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mailProject.Models
+{
+    public class Email
+    {
+        public string baslik { get; set; }
+        public string icerik { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+    }
+}
